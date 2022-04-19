a9c094e5af469e68ea440ff0c223d171d89d4998
raresbancescu <75622881+raresbancescu@users.noreply.github.com>
2022-04-19 16:55:59 +0300

    carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import "../css/Home.css"
+import Typography from "@mui/material/Typography";
+import r1 from "../pictures/recom1.jpg";
+import r2 from "../pictures/recom2.jpg";
+import r3 from "../pictures/recom3.jpg";
+import r4 from "../pictures/movie.png";
+import { Box } from "@mui/system";
+import CustomPopup from "../components/CustomPopUp";
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
@@ -9,7 +17,7 @@ const responsive = {
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3
+    items: 4
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
@@ -24,14 +32,67 @@ const responsive = {
 const Home = () => {
   return <>
  
- 
+  <Box sx={{width:"100%", backgroundImage:"../pictures/movie.png"}}>
+    
+  </Box>
 
- 
-        <Carousel responsive={responsive}>
-            <div>Item 1</div>
-            <div>Item 2</div>
-            <div>Item 3</div>
-            <div>Item 4</div>
+ <Typography
+        sx={{
+          fontFamily: "Trispace",
+          color: "#F9F871",
+          fontWeight: "bolder",
+          fontSize: "20px",
+          marginTop: "8vh",
+          backgroundColor: "#482884",
+          paddingBottom: "20px",
+          paddingLeft: "20px",
+          paddingTop: "20px"
+        }}
+      >
+        Action non-stop:
+      </Typography>
+        <Carousel className="carusel"
+       
+        
+        responsive={responsive}>
+           <img src={r1}></img>
+           <img src={r2}></img>
+           <img src={r3}></img>
+           <img src={r1}></img>
+           <img src={r2}></img>
+           <img src={r3}></img>
+           <img src={r1}></img>
+           <img src={r2}></img> 
+        </Carousel>  
+
+
+        <Typography
+        sx={{
+          fontFamily: "Trispace",
+          color: "#F9F871",
+          fontWeight: "bolder",
+          fontSize: "20px",
+          marginTop: "8vh",
+          backgroundColor: "#482884",
+          paddingBottom: "20px",
+          paddingLeft: "20px",
+          paddingTop: "20px"
+        }}
+      >
+        Not for the faint hearth
+      </Typography>
+        <Carousel className="carusel"
+       
+        
+        responsive={responsive}>
+           <img src={r1}></img>
+           <img src={r2}></img>
+           <img src={r3}></img>
+           <img src={r1}></img>
+           <img src={r2}></img>
+           <img src={r3}></img>
+           <img src={r1}></img>
+           <img src={r2}></img> 
         </Carousel>  
   </>;
 };
