a7cd364118846e59b02adf20fa1d1565300869b2
Kissa26 <79217057+Kissa26@users.noreply.github.com>
2022-04-19 17:22:33 +0300

    modificari

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<Login />} />
+        <Route index element={<Landing />} />
         <Route path="home" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
