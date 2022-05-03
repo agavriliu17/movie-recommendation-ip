9553f5e6122b1c7828bc77b656dff5d9f1380b03
Alin Gavriliu <alin_gavriliu@yahoo.com>
2022-05-03 11:35:25 +0300

    Added example for comment structure

---
+++ src/resources/comments.js
@@ -0,0 +1,9 @@
+export const comments = [
+  {
+    id: 0,
+    user: "testUser",
+    message: "Awesome movie",
+    likes: 231,
+    date: "Tue May 03 2022 11:31:56 GMT+0300 (Eastern European Summer Time)",
+  },
+];
