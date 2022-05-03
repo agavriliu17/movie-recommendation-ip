6b6866c57245c73b973160ed4dfa4ab2bdf94e98
PopaRazvan <poparazvan2001@yahoo.com>
2022-05-03 13:53:22 +0300

    Vizualizarea comentariilor

---
+++ src/pages/ListComments.js
@@ -0,0 +1,71 @@
+import { IconButton, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import React from "react";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import { useState, useEffect } from "react";
+import { comments as getCommets } from "../resources/comments";
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+
+const Comments = ({ currentUserId }) => {
+    const [backendComments, setBackendCommets] = useState([]);
+
+
+    console.log("backendComments", backendComments);
+
+    useEffect(() => {
+        getCommets().then((data) => {
+            setBackendCommets(data);
+        });
+    }, []);
+    return (
+        <Paper>
+            Comments
+            {backendComments.map((comment) => (
+                <Container
+                    sx={{
+                        display: "flex",
+                        marginTop: "28px",
+                    }}>
+                    <Box>
+
+                        <Box>{comment.date}</Box>
+                        <Box
+                            sx={{
+                                marginRight: "8px",
+                                fontSize: "20px",
+                            }}>
+                            {comment.user}
+                        </Box>
+
+                        <Box
+                            sx={{
+
+                            }}>{comment.message}</Box>
+
+
+                        <IconButton>
+                            <ThumbUpAltIcon
+                           
+
+                            onClick={() => {
+                                comment.likes+=1;
+                            }} />
+                            </IconButton>
+
+
+
+                        {comment.likes}
+
+
+
+                    </Box>
+
+                </Container>
+            ))}
+        </Paper>
+
+    );
+}
+
+export default Comments;
--- src/resources/comments.js
+++ src/resources/comments.js
@@ -1,9 +1,25 @@
-export const comments = [
+export const comments = async()=>{
+  return[
   {
     id: 0,
-    user: "testUser",
+    user: "testUser1",
     message: "Awesome movie",
-    likes: 231,
+    likes: 1,
     date: "Tue May 03 2022 11:31:56 GMT+0300 (Eastern European Summer Time)",
   },
+  {
+    id: 1,
+    user: "testUser2",
+    message: "Bad movie",
+    likes: 3,
+    date: "Tue May 03 2022 11:31:00 GMT+0300 (Eastern European Summer Time)",
+  },
+  {
+    id: 2,
+    user: "testUser3",
+    message: "Awesome movie",
+    likes: 2,
+    date: "Tue May 03 2022 11:31:42 GMT+0300 (Eastern European Summer Time)",
+  },
 ];
+}
