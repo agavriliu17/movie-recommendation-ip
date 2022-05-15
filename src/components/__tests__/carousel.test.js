601c85b21e5d72a43fd06dd77fc8c32f76b612d6
raresbancescu <75622881+raresbancescu@users.noreply.github.com>
2022-05-15 14:58:32 +0300

    Created the test folder

---
+++ src/components/__tests__/carousel.test.js
@@ -0,0 +1,13 @@
+import {render,screen,cleanup} from '@testing-library/react'
+import Hover from '../carousel/Hover'
+
+
+afterEach(()=>{
+    cleanup();
+});
+
+test('test',() =>{
+    expect(true).toBe(true);
+
+})
+
